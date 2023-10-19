c5af7e106d251017750c5dedae063a77ae0c376c
Marguerite Kennedy <mkennedy@enviroissues.com>
2023-10-19 08:50:54 -0700

    progess with file upload

--- routes/closureRoutes.js
+++ routes/closureRoutes.js
@@ -1,191 +1,85 @@
-// const express = require("express");
-// const router = express.Router();
-// const multer = require("multer");
-// const path = require("path");
-// const Closure = require("../models/Project");
-// const { uploadToAzure } = require("../utils/azureUpload");
-
-// const storage = multer.diskStorage({
-//   destination: function (e, r, t) {
-//     t(null, "uploads/");
-//   },
-//   filename: function (e, r, t) {
-//     t(null, Date.now() + "-" + r.originalname);
-//   },
-// });
-
-// function fileFilter(req, file, cb) {
-//   const filetypes = /jpeg|jpg|png|gif/;
-//   const mimetype = filetypes.test(file.mimetype);
-//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-//   if (mimetype && extname) {
-//     return cb(null, true);
-//   }
-
-//   cb("Error: File upload only supports the following filetypes - " + filetypes);
-// }
-
-// const upload = multer({
-//   storage: storage,
-//   fileFilter: fileFilter,
-// });
-
-// router.put("/:id", (async (e, r) => {
-//   upload.single('image')(e, r, async (err) => {
-//     if (err instanceof multer.MulterError) {
-//       return r.status(500).json({ error: err.message });
-//     } else if (err) {
-//       return r.status(500).json({ error: err });
-//     }
-
-//     try {
-//       const t = await Closure.findById(e.params.id);
-//       if (!t) return r.status(404).json({ error: "Closure not found" });
-
-//       Object.keys(e.body).forEach((r => {
-//         if (e.body[r] && t[r] !== undefined) {
-//           t[r] = e.body[r];
-//         }
-//       }));
-
-//       ["startDate", "endDate", "postDate", "removeDate", "createdAt", "updatedAt"].forEach((r => {
-//         if (e.body[r]) {
-//           t[r] = new Date(e.body[r]);
-//         }
-//       }));
-
-//       if (e.file) {
-//         const r = await uploadToAzure(e.file.buffer, e.file.originalname);
-//         t.imageUrl = r;
-//       }
-
-//       ["timingFeatures", "activityType", "impactType"].forEach((r => {
-//         if (e.body[r] && typeof e.body[r] === "string") {
-//           t[r] = e.body[r].split(",").map(e => e.trim());
-//         }
-//       }));
-
-//       if (e.body.mapData) {
-//         t.mapData = JSON.parse(e.body.mapData);
-//       }
-
-//       await t.save();
-//       r.json(t);
-//     } catch (e) {
-//       r.status(500).json({ error: "Internal server error while updating closure." });
-//     }
-//   });
-// }));
-
-// router.delete("/:id", (async (e, r) => {
-//   try {
-//     const t = await Closure.findByIdAndDelete(e.params.id);
-//     if (!t) return r.status(404).json({ error: "Closure not found" });
-//     r.json(t);
-//   } catch (e) {
-//     r.status(500).json({ error: "Internal server error while deleting closure." });
-//   }
-// }));
-
-// module.exports = router;
-
-
-
-
-
-
-
-
-
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
-const Closure = require("../models/Project");
+const Project = require('../models/Project');
 const { uploadToAzure } = require("../utils/azureUpload");
 
-const storage = multer.diskStorage({
-  destination: function (e, r, t) {
-    t(null, "uploads/");
-  },
-  filename: function (e, r, t) {
-    t(null, Date.now() + "-" + r.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "uploads/");
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + "-" + file.originalname);
+//     }
+// });
+
+const storage = multer.memoryStorage();
 
 function fileFilter(req, file, cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
-  const mimetype = filetypes.test(file.mimetype);
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  }
-
-  cb("Error: File upload only supports the following filetypes - " + filetypes);
+    const allowedExtensions = /jpeg|jpg|png|gif/;
+    const mimetypeCheck = allowedExtensions.test(file.mimetype);
+    const extnameCheck = allowedExtensions.test(path.extname(file.originalname).toLowerCase());
+    if (mimetypeCheck && extnameCheck) {
+        return cb(null, true);
+    }
+    cb(new Error("Error: File upload only supports the following filetypes - " + allowedExtensions));
 }
 
-const upload = multer({
-  storage: storage,
-  fileFilter: fileFilter,
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+router.put("/:id", upload.single("image"), async (req, res) => {
+    try {
+        const project = await ProjectPr.findById(req.params.id);
+        if (!project) {
+            return res.status(404).json({ error: "Closure not found" });
+        }
+
+        Object.keys(req.body).forEach(key => {
+            if (req.body[key] && project[key] !== undefined) {
+                project[key] = req.body[key];
+            }
+        });
+
+        ["startDate", "endDate", "postDate", "removeDate", "createdAt", "updatedAt"].forEach(dateKey => {
+            if (req.body[dateKey]) {
+                project[dateKey] = new Date(req.body[dateKey]);
+            }
+        });
+
+        if (req.file) {
+            const imageUrl = await uploadToAzure(req.file.buffer, req.file.originalname);
+            project.imageUrl = imageUrl;
+        }
+
+        ["timingFeatures", "activityType", "impactType"].forEach(key => {
+            if (req.body[key] && typeof req.body[key] === "string") {
+                project[key] = req.body[key].split(",").map(item => item.trim());
+            }
+        });
+
+        if (req.body.mapData) {
+            project.mapData = JSON.parse(req.body.mapData);
+        }
+
+        await project.save();
+        res.json(project);
+    }  catch (error) {
+        console.error("Error while updating closure:", error);
+        res.status(500).json({ error: "Internal server error while updating closure.", detailedError: error.message });
+    }
 });
 
-router.put("/:id", (async (e, r) => {
-  upload.single('image')(e, r, async (err) => {
-    if (err instanceof multer.MulterError) {
-      return r.status(500).json({ error: err.message });
-    } else if (err) {
-      return r.status(500).json({ error: err });
-    }
-
+router.delete("/:id", async (req, res) => {
     try {
-      const t = await Closure.findById(e.params.id);
-      if (!t) return r.status(404).json({ error: "Closure not found" });
-
-      Object.keys(e.body).forEach((r => {
-        if (e.body[r] && t[r] !== undefined) {
-          t[r] = e.body[r];
+        const project = await Project.findByIdAndDelete(req.params.id);
+        if (!project) {
+            return res.status(404).json({ error: "Closure not found" });
         }
-      }));
-
-      ["startDate", "endDate", "postDate", "removeDate", "createdAt", "updatedAt"].forEach((r => {
-        if (e.body[r]) {
-          t[r] = new Date(e.body[r]);
-        }
-      }));
-
-      if (e.file) {
-        const r = await uploadToAzure(e.file.buffer, e.file.originalname);
-        t.imageUrl = r;
-      }
-
-      ["timingFeatures", "activityType", "impactType"].forEach((r => {
-        if (e.body[r] && typeof e.body[r] === "string") {
-          t[r] = e.body[r].split(",").map(e => e.trim());
-        }
-      }));
-
-      if (e.body.mapData) {
-        t.mapData = JSON.parse(e.body.mapData);
-      }
-
-      await t.save();
-      r.json(t);
-    } catch (e) {
-      r.status(500).json({ error: "Internal server error while updating closure." });
+        res.json(project);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error while deleting closure." });
     }
-  });
-}));
-
-router.delete("/:id", (async (e, r) => {
-  try {
-    const t = await Closure.findByIdAndDelete(e.params.id);
-    if (!t) return r.status(404).json({ error: "Closure not found" });
-    r.json(t);
-  } catch (e) {
-    r.status(500).json({ error: "Internal server error while deleting closure." });
-  }
-}));
+});
 
 module.exports = router;
--- utils/azureUpload.js
+++ utils/azureUpload.js
@@ -1,3 +1,51 @@
+const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
+
+const accountName = 'sr520construction';
+const accountKey = process.env.AZURE_STORAGE_ACCOUNT_KEY;
+const containerName = '520-uploads';
+
+const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
+const blobServiceClient = new BlobServiceClient(
+  `https://${accountName}.blob.core.windows.net`,
+  sharedKeyCredential
+);
+
+// Create a container client
+const containerClient = blobServiceClient.getContainerClient(containerName);
+
+// Utility function to determine the MIME type based on the file extension
+function getMimeType(fileName) {
+    const extension = fileName.split('.').pop().toLowerCase();
+    const mimeTypes = {
+        'png': 'image/png',
+        'jpg': 'image/jpeg',
+        'jpeg': 'image/jpeg',
+        'gif': 'image/gif',
+        'pdf': 'application/pdf'
+    };
+    return mimeTypes[extension] || 'application/octet-stream';
+}
+
+const uploadToAzure = async (buffer, fileName) => {
+    const contentType = getMimeType(fileName);
+    const blockBlobClient = containerClient.getBlockBlobClient(fileName);
+    const uploadOptions = {
+        blobHTTPHeaders: {
+            blobContentType: contentType,
+            blobContentDisposition: `inline; filename=${fileName}`
+        }
+    };
+    await blockBlobClient.uploadData(buffer, uploadOptions);
+    return blockBlobClient.url;
+};
+
+module.exports = {
+    uploadToAzure
+};
+
+
+
+
 // const { BlobServiceClient, StorageSharedKeyCredential } = require('@azure/storage-blob');
 
 // const accountName = 'sr520construction';
@@ -50,35 +98,35 @@
 //     deleteFromAzure
 // };
 
-const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
+// const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
 
-const accountName = "sr520construction";
-const accountKey = process.env.AZURE_STORAGE_ACCOUNT_KEY;
-const containerName = "520-uploads";
+// const accountName = "sr520construction";
+// const accountKey = process.env.AZURE_STORAGE_ACCOUNT_KEY;
+// const containerName = "520-uploads";
 
-const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
-const blobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net`, sharedKeyCredential);
-const containerClient = blobServiceClient.getContainerClient(containerName);
+// const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
+// const blobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net`, sharedKeyCredential);
+// const containerClient = blobServiceClient.getContainerClient(containerName);
 
-const uploadToAzure = async (data, filename) => {
-    const blobClient = containerClient.getBlockBlobClient(filename);
-    const uploadOptions = {
-        blobHTTPHeaders: {
-            blobContentType: "application/octet-stream",
-            blobContentDisposition: `inline; filename=${filename}`
-        }
-    };
+// const uploadToAzure = async (data, filename) => {
+//     const blobClient = containerClient.getBlockBlobClient(filename);
+//     const uploadOptions = {
+//         blobHTTPHeaders: {
+//             blobContentType: "application/octet-stream",
+//             blobContentDisposition: `inline; filename=${filename}`
+//         }
+//     };
 
-    await blobClient.uploadData(data, uploadOptions);
-    return blobClient.url;
-};
+//     await blobClient.uploadData(data, uploadOptions);
+//     return blobClient.url;
+// };
 
-const deleteFromAzure = async filename => {
-    const blobClient = containerClient.getBlockBlobClient(filename);
-    await blobClient.delete();
-};
+// const deleteFromAzure = async filename => {
+//     const blobClient = containerClient.getBlockBlobClient(filename);
+//     await blobClient.delete();
+// };
 
-module.exports = {
-    uploadToAzure,
-    deleteFromAzure
-};
+// module.exports = {
+//     uploadToAzure,
+//     deleteFromAzure
+// };
