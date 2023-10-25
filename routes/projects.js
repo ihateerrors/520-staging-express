7b580d6db99d0020822dad74ef8a96be0cac2585
Marguerite Kennedy <mkennedy@enviroissues.com>
2023-10-25 08:39:38 -0700

    going back to other branch

--- routes/projects.js
+++ routes/projects.js
@@ -28,6 +28,38 @@ router.post('/upload-pdf', upload.fields([{ name: 'montlakeNoiseReport' }, { nam
     res.redirect('/dashboard'); // Or wherever you'd like to redirect after a successful upload
 });
 
+router.get("/dashboard", ensureAuthenticated, async (req, res) => {
+    try {
+        const projects = await Project.find({});
+
+        // Fetching the enum values from the Project model-- the caster method below is baked into the library functions
+        const activityTypeEnums = Project.schema.path('activityType').caster.enumValues;
+        const timingFeaturesEnums = Project.schema.path('timingFeatures').caster.enumValues;
+        const impactTypeEnums = Project.schema.path('impactType').caster.enumValues;
+
+        // Rendering enum arrays to the Dashboard 
+        if (projects.length === 0) {
+            res.render("dashboard", {
+                projects: [],
+                allActivityTypes: activityTypeEnums,
+                allTimingFeatures: timingFeaturesEnums,
+                allImpactTypes: impactTypeEnums,
+                message: "No projects available."
+            });
+        } else {
+            res.render("dashboard", {
+                projects: projects,
+                allActivityTypes: activityTypeEnums,
+                allTimingFeatures: timingFeaturesEnums,
+                allImpactTypes: impactTypeEnums
+            });
+        }
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
 
 router.get('/api/test', (req, res) => res.send('Test successful!'));
 
