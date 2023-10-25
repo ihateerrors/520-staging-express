26eac74e888289805267477887d2e8a7f7a467a2
Marguerite Kennedy <mkennedy@enviroissues.com>
2023-10-25 15:48:10 -0700

    change routes for closures

---
+++ routes/projects-stash1025.js
@@ -0,0 +1,266 @@
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const router = express.Router();
+const { uploadToAzure } = require('../utils/azureUpload');
+const Project = require('../models/Project');
+const formatDate = require('../utils/dateHelpers');
+const ensureAuthenticated = require('../middlewares/auth');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.get('/pdf-upload', (req, res) => {
+    res.render('pdf-upload');
+});
+
+// Handle PDF uploads
+router.post('/upload-pdf', upload.fields([{ name: 'montlakeNoiseReport' }, { name: 'i5NoiseReport' }]), async (req, res) => {
+    if (req.files.montlakeNoiseReport) {
+        const file = req.files.montlakeNoiseReport[0];
+        await uploadToAzure(file.buffer, file.originalname);
+    }
+
+    if (req.files.i5NoiseReport) {
+        const file = req.files.i5NoiseReport[0];
+        await uploadToAzure(file.buffer, file.originalname);
+    }
+
+    res.redirect('/dashboard'); // Or wherever you'd like to redirect after a successful upload
+});
+
+
+router.get('/api/test', (req, res) => res.send('Test successful!'));
+
+router.get('/api/projects/mapData', async (req, res) => {
+    try {
+        // Extract filters from query parameters
+        const { startDate, endDate, types } = req.query;
+
+        // Convert types string to an array
+        const activityTypes = types ? types.split(",") : [];
+
+        // Formulate the query object
+        const query = {};
+
+        if (activityTypes.length && !activityTypes.includes('all')) {
+            query.activityType = { $in: activityTypes };
+        }
+
+        if (startDate && endDate) {
+            query.startDate = { $gte: new Date(startDate) };
+            query.endDate = { $lte: new Date(endDate) };
+        }
+
+        // Fetch both mapData and activityType from the database
+        const projects = await Project.find(query).select('mapData activityType');
+        
+        res.json({ projects });
+    } catch (error) {
+        console.error("Error fetching project data:", error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.post('/api/projects/mapData', async (req, res) => {
+    const filters = req.body;
+    const activityTypes = filters.activityTypes || [];
+
+    let query = {};
+
+    if (activityTypes.length && !activityTypes.includes('all')) {
+        query.activityType = { $in: activityTypes };
+    }
+
+    query.startDate = { $gte: new Date(filters.startDate) };
+    query.endDate = { $lte: new Date(filters.endDate) };
+
+    try {
+        // Fetch both mapData and activityType from the database
+        const projects = await Project.find(query).select('mapData activityType');
+        res.json({ projects });
+    } catch (error) {
+        console.error('Error fetching filtered projects:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+router.get('/projects/:projectId', async (req, res) => {
+    try {
+        const projectId = req.params.projectId;
+
+        if (!mongoose.Types.ObjectId.isValid(projectId)) {
+            res.status(400).send('Invalid Project ID');
+            return;
+        }
+
+        const project = await Project.findById(projectId);
+
+        if (!project) {
+            res.status(404).send('Project not found');
+            return;
+        }
+
+        res.render('projectDetails', { project, formatDate });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/api/projects', async (req, res) => {
+    console.log("Entered /api/projects route");
+    try {
+        let filters = {};
+
+        if (req.query.startDate) filters.startDate = { $gte: new Date(req.query.startDate) };
+        if (req.query.endDate) filters.endDate = { $lte: new Date(req.query.endDate) };
+        if (req.query.types) filters.activityType = { $in: req.query.types.split(",") };
+        if (req.query.cameras) filters.cameras = req.query.cameras === 'true';
+
+        const projects = await Project.find(filters);
+        res.json(projects);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/projectForm', ensureAuthenticated, (req, res) => {
+    res.render('projectForm', { 
+        success_msg: req.flash('success_msg'), 
+        error_msg: req.flash('error_msg')
+    });
+});
+
+
+router.get("/dashboard", ensureAuthenticated, async (req, res) => {
+    try {
+        const closures = await Project.find({});
+
+        // Fetching the enum values from the Project model-- the caster method below is baked into the library functions
+        const activityTypeEnums = Project.schema.path('activityType').caster.enumValues;
+        const timingFeaturesEnums = Project.schema.path('timingFeatures').caster.enumValues;
+        const impactTypeEnums = Project.schema.path('impactType').caster.enumValues;
+
+        // Rendering enum arrays to the Dashboard 
+        if (closures.length === 0) {
+            res.render("dashboard", {
+                closures: [],
+                allActivityTypes: activityTypeEnums,
+                allTimingFeatures: timingFeaturesEnums,
+                allImpactTypes: impactTypeEnums,
+                message: "No closures available."
+            });
+        } else {
+            res.render("dashboard", {
+                closures: closures,
+                allActivityTypes: activityTypeEnums,
+                allTimingFeatures: timingFeaturesEnums,
+                allImpactTypes: impactTypeEnums
+            });
+        }
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+router.post('/api/projects', upload.single('file'), async (req, res) => {
+    try {
+        // Handling file upload
+        if (req.file && req.file.buffer) {
+            const azureFileUrl = await uploadToAzure(req.file.buffer, req.file.originalname);
+            console.log(azureFileUrl);
+            req.body.imageUrl = azureFileUrl;  // Saves the URL to the image in the database
+        }
+        // Creating and saving the project
+        const project = new Project(req.body);
+        await project.save();
+        req.flash('success_msg', 'Construction event created successfully.');
+        res.redirect('/projectForm');
+    } catch (err) {
+        console.error('Error:', err);
+        
+        // Handle validation error
+        if (err.name === 'ValidationError') {
+            let errorMessages = Object.values(err.errors).map(e => e.message);
+            req.flash('error_msg', 'Error in creating the event: ' + errorMessages.join(', '));
+            res.redirect('/projectForm');
+        } else {
+            req.flash('error_msg', 'Internal server error, yo');
+            res.redirect('/projectForm');
+        }
+    }
+});
+
+
+router.get('/latest-closures', async (req, res) => {
+    try {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        const closures = await Project.find({
+            endDate: { $gte: today },
+            postDate: { $lte: today }
+        })
+        .sort({ postDate: -1 }) // This sorts by postDate in descending order.
+        .limit(8)
+        .exec();
+
+        res.json(closures);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+const fetchRecentClosures = async () => {
+    try {
+        return await Project.find({}).limit(10);
+    } catch (error) {
+        console.error("Error fetching recent closures:", error);
+        throw error;
+    }
+};
+
+
+router.get('/api/projects/:projectId/mapData', async (req, res) => {
+    try {
+        const projectId = req.params.projectId;
+
+        if (!mongoose.Types.ObjectId.isValid(projectId)) {
+            res.status(400).send('Invalid Project ID');
+            return;
+        }
+
+        const project = await Project.findById(projectId);
+
+        if (!project) {
+            res.status(404).send('Project not found');
+            return;
+        }
+
+        if (!project.mapData) {
+            res.status(404).send('Map data not found for the project');
+            return;
+        }
+
+        res.json({
+            _id: project._id,
+            mapData: project.mapData
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+module.exports = {
+    router,
+    fetchRecentClosures,
+    upload
+};
--- routes/projects.js
+++ routes/projects.js
@@ -1,262 +1,200 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const router = express.Router();
-const { uploadToAzure } = require('../utils/azureUpload');
-const Project = require('../models/Project');
-const formatDate = require('../utils/dateHelpers');
-const ensureAuthenticated = require('../middlewares/auth');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-router.get('/pdf-upload', (req, res) => {
-    res.render('pdf-upload');
-});
-
-// Handle PDF uploads
-router.post('/upload-pdf', upload.fields([{ name: 'montlakeNoiseReport' }, { name: 'i5NoiseReport' }]), async (req, res) => {
-    if (req.files.montlakeNoiseReport) {
-        const file = req.files.montlakeNoiseReport[0];
-        await uploadToAzure(file.buffer, file.originalname);
+const express = require("express"),
+  mongoose = require("mongoose"),
+  multer = require("multer"),
+  router = express.Router(),
+  { uploadToAzure: uploadToAzure } = require("../utils/azureUpload"),
+  Project = require("../models/Project"),
+  formatDate = require("../utils/dateHelpers"),
+  ensureAuthenticated = require("../middlewares/auth"),
+  storage = multer.memoryStorage(),
+  upload = multer({ storage: storage });
+  
+router.get("/pdf-upload", (e, t) => {
+  t.render("pdf-upload");
+}),
+  router.post(
+    "/upload-pdf",
+    upload.fields([{ name: "montlakeNoiseReport" }, { name: "i5NoiseReport" }]),
+    async (e, t) => {
+      if (e.files.montlakeNoiseReport) {
+        const t = e.files.montlakeNoiseReport[0];
+        await uploadToAzure(t.buffer, t.originalname);
+      }
+      if (e.files.i5NoiseReport) {
+        const t = e.files.i5NoiseReport[0];
+        await uploadToAzure(t.buffer, t.originalname);
+      }
+      t.redirect("/dashboard");
     }
-
-    if (req.files.i5NoiseReport) {
-        const file = req.files.i5NoiseReport[0];
-        await uploadToAzure(file.buffer, file.originalname);
-    }
-
-    res.redirect('/dashboard'); // Or wherever you'd like to redirect after a successful upload
-});
-
-
-router.get('/api/test', (req, res) => res.send('Test successful!'));
-
-router.get('/api/projects/mapData', async (req, res) => {
+  ),
+  router.get("/api/test", (e, t) => t.send("Test successful!")),
+  router.get("/api/projects/mapData", async (e, t) => {
     try {
-        // Extract filters from query parameters
-        const { startDate, endDate, types } = req.query;
-
-        // Convert types string to an array
-        const activityTypes = types ? types.split(",") : [];
-
-        // Formulate the query object
-        const query = {};
-
-        if (activityTypes.length && !activityTypes.includes('all')) {
-            query.activityType = { $in: activityTypes };
-        }
-
-        if (startDate && endDate) {
-            query.startDate = { $gte: new Date(startDate) };
-            query.endDate = { $lte: new Date(endDate) };
-        }
-
-        // Fetch both mapData and activityType from the database
-        const projects = await Project.find(query).select('mapData activityType');
-        
-        res.json({ projects });
-    } catch (error) {
-        console.error("Error fetching project data:", error);
-        res.status(500).json({ error: 'Internal Server Error' });
+      const { startDate: r, endDate: a, types: s } = e.query,
+        o = s ? s.split(",") : [],
+        n = {};
+      o.length && !o.includes("all") && (n.activityType = { $in: o }),
+        r &&
+          a &&
+          ((n.startDate = { $gte: new Date(r) }),
+          (n.endDate = { $lte: new Date(a) }));
+      const c = await Project.find(n).select("mapData activityType");
+      t.json({ projects: c });
+    } catch (e) {
+      t.status(500).json({ error: "Internal Server Error" });
     }
-});
-
-router.post('/api/projects/mapData', async (req, res) => {
-    const filters = req.body;
-    const activityTypes = filters.activityTypes || [];
-
-    let query = {};
-
-    if (activityTypes.length && !activityTypes.includes('all')) {
-        query.activityType = { $in: activityTypes };
-    }
-
-    query.startDate = { $gte: new Date(filters.startDate) };
-    query.endDate = { $lte: new Date(filters.endDate) };
-
+  }),
+  router.post("/api/projects/mapData", async (e, t) => {
+    const r = e.body,
+      a = r.activityTypes || [];
+    let s = {};
+    a.length && !a.includes("all") && (s.activityType = { $in: a }),
+      (s.startDate = { $gte: new Date(r.startDate) }),
+      (s.endDate = { $lte: new Date(r.endDate) });
     try {
-        // Fetch both mapData and activityType from the database
-        const projects = await Project.find(query).select('mapData activityType');
-        res.json({ projects });
-    } catch (error) {
-        console.error('Error fetching filtered projects:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+      const e = await Project.find(s).select("mapData activityType");
+      t.json({ projects: e });
+    } catch (e) {
+      t.status(500).json({ error: "Internal Server Error" });
     }
-});
+  }),
 
-
-router.get('/projects/:projectId', async (req, res) => {
+//   this is the route 
+  router.get("/projects/:projectId", async (e, t) => {
     try {
-        const projectId = req.params.projectId;
+      const r = e.params.projectId;
 
-        if (!mongoose.Types.ObjectId.isValid(projectId)) {
-            res.status(400).send('Invalid Project ID');
-            return;
-        }
-
-        const project = await Project.findById(projectId);
-
-        if (!project) {
-            res.status(404).send('Project not found');
-            return;
-        }
-
-        res.render('projectDetails', { project, formatDate });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+      if (!mongoose.Types.ObjectId.isValid(r))
+        return void t.status(400).send("Invalid Project ID");
+      const a = await Project.findById(r);
+      if (!a) return void t.status(404).send("Project not found");
+      t.render("projectDetails", { closure: a, formatDate: formatDate });
+    } catch (e) {
+      t.status(500).send("Internal Server Error");
     }
-});
+  }),
+  
+//   this is the route END
 
-router.get('/api/projects', async (req, res) => {
-    console.log("Entered /api/projects route");
+
+
+
+  router.get("/api/projects", async (e, t) => {
     try {
-        let filters = {};
-
-        if (req.query.startDate) filters.startDate = { $gte: new Date(req.query.startDate) };
-        if (req.query.endDate) filters.endDate = { $lte: new Date(req.query.endDate) };
-        if (req.query.types) filters.activityType = { $in: req.query.types.split(",") };
-        if (req.query.cameras) filters.cameras = req.query.cameras === 'true';
-
-        const projects = await Project.find(filters);
-        res.json(projects);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+      let r = {};
+      e.query.startDate &&
+        (r.startDate = { $gte: new Date(e.query.startDate) }),
+        e.query.endDate && (r.endDate = { $lte: new Date(e.query.endDate) }),
+        e.query.types && (r.activityType = { $in: e.query.types.split(",") }),
+        e.query.cameras && (r.cameras = "true" === e.query.cameras);
+      const a = await Project.find(r);
+      t.json(a);
+    } catch (e) {
+      t.status(500).send("Internal Server Error");
     }
-});
-
-router.get('/projectForm', ensureAuthenticated, (req, res) => {
-    res.render('projectForm', { 
-        success_msg: req.flash('success_msg'), 
-        error_msg: req.flash('error_msg')
+  }),
+  router.get("/projectForm", ensureAuthenticated, (e, t) => {
+    t.render("projectForm", {
+      success_msg: e.flash("success_msg"),
+      error_msg: e.flash("error_msg"),
     });
-});
-
-
-router.get("/dashboard", ensureAuthenticated, async (req, res) => {
+  }),
+  router.get("/dashboard", ensureAuthenticated, async (e, t) => {
     try {
-        const closures = await Project.find({});
-
-        // Fetching the enum values from the Project model-- the caster method below is baked into the library functions
-        const activityTypeEnums = Project.schema.path('activityType').caster.enumValues;
-        const timingFeaturesEnums = Project.schema.path('timingFeatures').caster.enumValues;
-        const impactTypeEnums = Project.schema.path('impactType').caster.enumValues;
-
-        // Rendering enum arrays to the Dashboard 
-        if (closures.length === 0) {
-            res.render("dashboard", {
-                closures: [],
-                allActivityTypes: activityTypeEnums,
-                allTimingFeatures: timingFeaturesEnums,
-                allImpactTypes: impactTypeEnums,
-                message: "No closures available."
-            });
-        } else {
-            res.render("dashboard", {
-                closures: closures,
-                allActivityTypes: activityTypeEnums,
-                allTimingFeatures: timingFeaturesEnums,
-                allImpactTypes: impactTypeEnums
-            });
-        }
-    } catch (error) {
-        res.status(500).send("Internal Server Error");
+      const e = await Project.find({}),
+        r = Project.schema.path("activityType").caster.enumValues,
+        a = Project.schema.path("timingFeatures").caster.enumValues,
+        s = Project.schema.path("impactType").caster.enumValues;
+      0 === e.length
+        ? t.render("dashboard", {
+            closures: [],
+            allActivityTypes: r,
+            allTimingFeatures: a,
+            allImpactTypes: s,
+            message: "No closures available.",
+          })
+        : t.render("dashboard", {
+            closures: e,
+            allActivityTypes: r,
+            allTimingFeatures: a,
+            allImpactTypes: s,
+          });
+    } catch (e) {
+      t.status(500).send("Internal Server Error");
     }
-});
-
-
-router.post('/api/projects', upload.single('file'), async (req, res) => {
+  }),
+  router.post("/api/projects", upload.single("file"), async (e, t) => {
     try {
-        // Handling file upload
-        if (req.file && req.file.buffer) {
-            const azureFileUrl = await uploadToAzure(req.file.buffer, req.file.originalname);
-            console.log(azureFileUrl);
-            req.body.imageUrl = azureFileUrl;  // Saves the URL to the image in the database
-        }
-        // Creating and saving the project
-        const project = new Project(req.body);
-        await project.save();
-        req.flash('success_msg', 'Construction event created successfully.');
-        res.redirect('/projectForm');
-    } catch (err) {
-        console.error('Error:', err);
-        
-        // Handle validation error
-        if (err.name === 'ValidationError') {
-            let errorMessages = Object.values(err.errors).map(e => e.message);
-            req.flash('error_msg', 'Error in creating the event: ' + errorMessages.join(', '));
-            res.redirect('/projectForm');
-        } else {
-            req.flash('error_msg', 'Internal server error, yo');
-            res.redirect('/projectForm');
-        }
+      if (e.file && e.file.buffer) {
+        const t = await uploadToAzure(e.file.buffer, e.file.originalname);
+        e.body.imageUrl = t;
+      }
+      const r = new Project(e.body);
+      await r.save(),
+        e.flash("success_msg", "Construction event created successfully."),
+        t.redirect("/projectForm");
+    } catch (r) {
+      if ("ValidationError" === r.name) {
+        let a = Object.values(r.errors).map((e) => e.message);
+        e.flash("error_msg", "Error in creating the event: " + a.join(", ")),
+          t.redirect("/projectForm");
+      } else
+        e.flash("error_msg", "Internal server error, yo"),
+          t.redirect("/projectForm");
     }
-});
-
-
-router.get('/api/projects/:projectId/mapData', async (req, res) => {
+  }),
+  router.get("/latest-closures", async (e, t) => {
     try {
-        const projectId = req.params.projectId;
-
-        if (!mongoose.Types.ObjectId.isValid(projectId)) {
-            res.status(400).send('Invalid Project ID');
-            return;
-        }
-
-        const project = await Project.findById(projectId);
-
-        if (!project) {
-            res.status(404).send('Project not found');
-            return;
-        }
-
-        if (!project.mapData) {
-            res.status(404).send('Map data not found for the project');
-            return;
-        }
-
-        res.json({
-            _id: project._id,
-            mapData: project.mapData
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-router.get('/latest-closures', async (req, res) => {
-    try {
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
-
-        const closures = await Project.find({
-            endDate: { $gte: today },
-            postDate: { $lte: today }
-        })
-        .sort({ postDate: -1 }) // This sorts by postDate in descending order.
-        .limit(4)
+      const e = new Date();
+      e.setHours(0, 0, 0, 0);
+      const r = await Project.find({
+        endDate: { $gte: e },
+        postDate: { $lte: e },
+      })
+        .sort({ postDate: -1 })
+        .limit(8)
         .exec();
-
-        res.json(closures);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+      t.json(r);
+    } catch (e) {
+      t.status(500).send("Internal Server Error");
     }
-});
-
+  });
 const fetchRecentClosures = async () => {
-    try {
-        return await Project.find({}).limit(10);
-    } catch (error) {
-        console.error("Error fetching recent closures:", error);
-        throw error;
-    }
+  try {
+    return await Project.find({}).limit(10);
+  } catch (e) {
+    throw e;
+  }
 };
+router.get("/api/projects/:projectId/mapData", async (e, t) => {
+  try {
+    const r = e.params.projectId;
+    if (!mongoose.Types.ObjectId.isValid(r))
+      return void t.status(400).send("Invalid Project ID");
+    const a = await Project.findById(r);
+    if (!a) return void t.status(404).send("Project not found");
+    if (!a.mapData)
+      return void t.status(404).send("Map data not found for the project");
+    t.json({ _id: a._id, mapData: a.mapData });
+  } catch (e) {
+    t.status(500).send("Internal Server Error");
+  }
+}),
+  (module.exports = {
+    router: router,
+    fetchRecentClosures: fetchRecentClosures,
+    upload: upload,
+  });
 
-module.exports = {
-    router,
-    fetchRecentClosures,
-    upload
-};
+
+
+
+
+
+
+
+
+
+
+
+
