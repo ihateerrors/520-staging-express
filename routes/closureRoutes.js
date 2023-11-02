26d7ac372a3f0936eff7560ca3d2396031f50dec
Marguerite Kennedy <mkennedy@enviroissues.com>
2023-11-02 14:51:06 -0700

    i5 connection page Flickr carousel fix

--- routes/closureRoutes.js
+++ routes/closureRoutes.js
@@ -5,71 +5,149 @@ const path = require("path");
 const Project = require("../models/Project");
 const { uploadToAzure } = require("../utils/azureUpload");
 
-const storage = multer.memoryStorage();
-
-function fileFilter(e, r, t) {
-    const o = /jpeg|jpg|png|gif/;
-    const a = o.test(r.mimetype);
-    const s = o.test(path.extname(r.originalname).toLowerCase());
-
-    if (a && s) return t(null, true);
-
-    t(new Error("Error: File upload only supports the following filetypes - " + o));
+function fileFilter(req, file, cb) {
+    const allowedExtensions = /jpeg|jpg|png|gif/;
+    const mimetypeCheck = allowedExtensions.test(file.mimetype);
+    const extnameCheck = allowedExtensions.test(path.extname(file.originalname).toLowerCase());
+    if (mimetypeCheck && extnameCheck) {
+        return cb(null, true);
+    }
+    cb(new Error("Error: File upload only supports the following filetypes - " + allowedExtensions));
 }
 
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+// const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-router.put("/api/projects/:id", upload.single("image"), async (e, r) => {
+
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+// Express route example
+router.post('/upload', upload.single('file'), async (req, res) => {
     try {
-        const t = await Project.findById(e.params.id);
-        if (!t) return r.status(404).json({ error: "Closure not found" });
-
-        // Update regular properties
-        Object.keys(e.body).forEach(r => {
-            if (e.body[r] && t[r] !== undefined) {
-                t[r] = e.body[r];
-            }
-        });
-
-        // Handle dates
-        ["startDate", "endDate", "postDate", "removeDate", "createdAt", "updatedAt"].forEach(r => {
-            if (e.body[r]) {
-                t[r] = new Date(e.body[r]);
-            }
-        });
-
-        // Handle uploaded image
-        if (e.file) {
-            const imageUrl = await uploadToAzure(e.file.buffer, e.file.originalname);
-            t.imageUrl = imageUrl;
+        if (!req.file) {
+            return res.status(400).send('No file uploaded.');
         }
 
-        // Handle checkbox arrays
-        ["timingFeatures", "activityType", "impactType"].forEach(r => {
-            if (e.body[r]) {
-                t[r] = JSON.parse(e.body[r]);
-            }
-        });
+        const fileBuffer = req.file.buffer;
+        const fileName = req.file.originalname;
+        
+        const fileUrl = await uploadToAzure(fileBuffer, fileName);
 
-        // Handle mapData
-        if (e.body.mapData) {
-            t.mapData = JSON.parse(e.body.mapData);
-        }
-
-        await t.save();
-        r.json(t);
-    } catch (e) {
-        r.status(500).json({ error: "Internal server error while updating closure." });
+        res.send({ url: fileUrl });
+    } catch (err) {
+        res.status(500).send('Error uploading file.');
     }
 });
 
-router.delete("/:id", async (e, r) => {
+
+
+// const upload = multer({ storage: multer.memoryStorage(), fileFilter: fileFilter });
+
+
+// router.put("/api/projects/:projectId", upload.single("image"), async (req, res) => {
+//     try {
+//         const project = await Project.findOne({ projectId: req.params.projectId });
+//         if (!project) {
+//             return res.status(404).json({ error: "Closure not found" });
+//         }
+
+//         Object.keys(req.body).forEach(key => {
+//             if (req.body[key] && project[key] !== undefined) {
+//                 project[key] = req.body[key];
+//             }
+//         });
+
+//         ["startDate", "endDate", "postDate", "removeDate", "createdAt", "updatedAt"].forEach(dateKey => {
+//             if (req.body[dateKey]) {
+//                 project[dateKey] = new Date(req.body[dateKey]);
+//             }
+//         });
+
+//         if (req.file) {
+//             const imageUrl = await uploadToAzure(req.file.buffer, req.file.originalname);
+//             project.imageUrl = imageUrl;
+//         }
+
+//         ["timingFeatures", "activityType", "impactType"].forEach(key => {
+//             if (req.body[key] && typeof req.body[key] === "string") {
+//                 project[key] = req.body[key].split(",").map(item => item.trim());
+//             }
+//         });
+
+//         await project.save();
+//         res.json(project);
+//     }  catch (error) {
+//         console.error("Error while updating closure:", error);
+//         res.status(500).json({ error: "Internal server error while updating closure.", detailedError: error.message });
+//     }
+// });
+
+router.put("/api/projects/:id", upload.single("image"), async (req, res) => {
     try {
-        const t = await Project.findByIdAndDelete(e.params.id);
-        if (!t) return r.status(404).json({ error: "Closure not found" });
-        r.json(t);
-    } catch (e) {
-        r.status(500).json({ error: "Internal server error while deleting closure." });
+        // Find the project by the provided ID
+        const project = await Project.findById(req.params.id);
+
+        // If the project isn't found, respond with an error
+        if (!project) {
+            return res.status(404).json({ error: "Closure not found" });
+        }
+
+        // Loop through the request body to update the project fields
+        Object.keys(req.body).forEach((key) => {
+            if (req.body[key] && project[key] !== undefined) {
+                project[key] = req.body[key];
+            }
+        });
+
+        // Update date fields
+        ["startDate", "endDate", "postDate", "removeDate", "createdAt", "updatedAt"].forEach((dateField) => {
+            if (req.body[dateField]) {
+                project[dateField] = new Date(req.body[dateField]);
+            }
+        });
+
+        // If there's an uploaded file, save it to Azure and update the imageUrl
+        if (req.file) {
+            const imageUrl = await uploadToAzure(req.file.buffer, req.file.originalname);
+            project.imageUrl = imageUrl;
+        }
+
+        // Parse and update JSON fields
+        ["timingFeatures", "activityType", "impactType"].forEach((jsonField) => {
+            if (req.body[jsonField]) {
+                project[jsonField] = JSON.parse(req.body[jsonField]);
+            }
+        });
+
+        // Update map data
+        if (req.body.mapData) {
+            project.mapData = JSON.parse(req.body.mapData);
+        }
+
+        // Save the updated project
+        await project.save();
+
+        // Respond with the updated project
+        res.json(project);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error: "Internal server error while updating closure.",
+            detailedError: error.message
+        });
+    }
+});
+
+router.delete("/api/projects/:projectId", async (req, res) => {
+    try {
+        const project = await Project.findOneAndDelete({ projectId: req.params.projectId });
+        if (!project) {
+            return res.status(404).json({ error: "Closure not found" });
+        }
+        res.json(project);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error while deleting closure." });
     }
 });
 
