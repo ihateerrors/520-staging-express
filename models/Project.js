c43822a6ca86a72df71cf0c1fd17912597dc994c
ihateerrors <ihateerrors@enviroissues.com>
2023-11-13 19:22:55 +0000

    project.js updated

--- models/Project.js
+++ models/Project.js
@@ -1,97 +1,148 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const { customAlphabet } = require('nanoid');
 
-const projectSchema = new mongoose.Schema({
-    activityName: {
-        type: String,
-        required: true
-    },
-    startDate: {
-        type: Date,
-        required: true
-    },
-    endDate: {
-        type: Date,
-        required: false
-    },
-    time: {
-        type: String,
-        required: false
-    },
-    timingFeatures: [{
-        type: String,
-        enum: ["Around-the-clock", "Nighttime", "Daytime", "Overnight"]
-    }],
-    description: {
-        type: String,
-        required: false
-    },
-    trafficDescription: {
-        type: String,
-        required: false
-    },
-    activityType: [{
-        type: String,
-        enum: [
-            "Full highway closure",
-            "Partial highway closure",
-            "Street or lane closure",
-            "Trail closure",
-            "Ramp closure",
-            "High-impact construction",
-            "Cameras"
-        ]
-    }],
-    impactType: [{
-        type: String,
-        required: false,
-        enum: [
-            "Bike/pedestrian",
-            "Light",
-            "Traffic",
-            "Dust",
-            "Noise",
-            "Vibration"
-        ]
-    }],
-    // imageAlt: {
-    //     type: String,  
-    //     required: false
-    // },
-    imageUrl: {
-        type: String,  // saves to Azure database which generates the URL stored in MongoDB
-        required: false
-    },
-  
-    location: {
-        type: String,
-        required: false
-    },
+const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz';
+const nanoid = customAlphabet(alphabet, 10);
 
-    mapData: {
-        type: mongoose.Schema.Types.Mixed,
-        required: false
-    },
-    
-    bannerContent: {
-        type: String,
-        required: true,
-        enum: ['yes', 'no']  // It ensures only 'yes' or 'no' values are stored for this field
-    },
-    postDate: {
-        type: Date,
-        required: true
-    },
-    removeDate: {
-        type: Date,
-        default: null  // If no date is provided, null is set by default
-    },
-    contact: {
-        type: String,
-        enum: ['206-777-8885', '206-316-2559', '206-200-9484'],  
-        required: false
+/**
+ * Note: If you add to an enum, you must also add to the message.properties file
+ */
+
+const ProjectSchema = new mongoose.Schema({
+  activityName: {
+    type: String,
+    required: true
+  },
+  startDate: {
+    type: Date,
+    required: true
+  },
+  endDate: {
+    type: Date,
+    required: false
+  },
+  time: {
+    type: String,
+    required: false
+  },
+  timingFeatures: [{
+    type: String,
+    enum: ["aroundTheClock", "nighttime", "daytime", "overnight"]
+  }],
+  description: {
+    type: String,
+    required: false
+  },
+  trafficDescription: {
+    type: String,
+    required: false
+  },
+  activityType: [{
+    type: String,
+    enum: [
+      "fullHighway",
+      "partialHighway",
+      "streetAndLane",
+      "trail",
+      "ramp",
+      "highImpact"
+    ]
+  }],
+  impactType: [{
+    type: String,
+    required: false,
+    enum: [
+      "bikePedestrian",
+      "light",
+      "traffic",
+      "dust",
+      "noise",
+      "vibration"
+    ]
+  }],
+  mapFeatures: [{
+    type: String,
+    required: false,
+    enum: ["cameras"]
+  }],
+  imageUrl: {
+    type: String,  // saves to Azure database which generates the URL stored in MongoDB
+    required: false
+  },
+  location: {
+    type: String,
+    required: false
+  },
+  mapData: {
+    type: String,
+    validate: {
+      validator: function(value) {
+        try {
+          const mapDataObject = JSON.parse(value);
+          if (mapDataObject.type === 'FeatureCollection' && Array.isArray(mapDataObject.features)) {
+            return true;
+          }
+          return false;
+        } catch (error) {
+          return false;
+        }
+      },
+      message: 'Invalid mapData object',
     }
+  },
+  bannerContent: {
+    type: String,
+    required: true,
+    enum: ['yes', 'no']  // It ensures only 'yes' or 'no' values are stored for this field
+  },
+  postDate: {
+    type: Date,
+    required: true
+  },
+  removeDate: {
+    type: Date,
+    default: null  // If no date is provided, null is set by default
+  },
+  contact: {
+    type: String,
+    enum: ['montlake', 'i5', 'general'],
+    required: false
+  },
+  projectId: {
+    type: String,
+    unique: true,
+    default: function() {
+      return nanoid();
+    }
+  },
+  slug: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
-const Project = mongoose.model('Project', projectSchema);
+ProjectSchema.pre('validate', async function(next) {
+  // Generate a slug from the title
+  const originalSlug = slugify(this.activityName, { lower: true, strict: true });
+  // Check if the generated slug already exists in the database
+  let count = 1;
+  let slug = originalSlug;
+  while (true) {
+    const existingDocument = await this.constructor.findOne({ slug });
+    if (!existingDocument || existingDocument._id.equals(this._id)) {
+      // No document with this slug exists, or it exists but it's the same document
+      break;
+    }
+    // Append a number to the original slug to make it unique
+    count++;
+    slug = `${originalSlug}-${count}`;
+  }
+  // Set the unique slug
+  this.slug = slug;
+  next();
+});
+
+const Project = mongoose.model('Project', ProjectSchema);
 
 module.exports = Project;
